b839fd35ebe1c76033d98d407d593e6c8ae95d9e
H-kleiber <limnuev25@gmail.com>
2023-06-05 17:08:14 -0500

    ejer arrys mostrar

--- app.js
+++ app.js
@@ -68,7 +68,8 @@ let dogs = [
 const main =document.querySelector('.container');
 const fragmento=document.createDocumentFragment();
 
-const render =()=>{
+function render (){
+
  dogs.forEach(element => {
     
     const div=document.createElement('div');
@@ -93,50 +94,92 @@ const render =()=>{
     div.appendChild(telefono)
     div.appendChild(pais)
 
-    botonoes.innerHTML=`<div class="editar">edit</div>
+    botonoes.innerHTML=`<div class="editar">editar</div>
+                        <div id='${element.id}'class="id_card">${element.id}</div>
                         <div class="delete">Delete</div>`
 
     img.innerHTML=`<img class="img_dog-size" src="${element.img}" alt="">`
-    
     name.textContent=element.name
     telefono.textContent=element.telefono
     pais.textContent=element.pais
     
     main.appendChild(div)    
+
+    
  });
  main.appendChild(fragmento);
+ 
 
 }
-render();
- 
-const boton_eliminar=document.querySelector('.delete').classList.value
+
 
 main.addEventListener('click', function(e) {
-    if (e.target.classList.value===boton_eliminar){
+    
+    const boton_eliminar=document.querySelector('.delete').classList.value;
+    const aceptar=document.querySelector('.aceptar').classList.value;; 
+    const cancelar=document.querySelector('.cancelar').classList.value;
+    const editar=document.querySelector('.editar').classList.value;
+    const ventana_eliminar=document.querySelector('#default_eliminar_id');
+    const ventana__edit=document.querySelector('#ventana__edit_id');
+    const ventana__edit_guardar=document.querySelector('.ventana__edit_guardar').classList.value;
+    const ventana__edit_cancelar=document.querySelector('.ventana__edit_cancelar').classList.value;
+    
+    let contador=0;
+    
+    if(e.target.classList.value===boton_eliminar){
         ventana_eliminar.className = "eliminar";
+        elemento_temp=e.target;
+        
+    }if(e.target.classList.value===editar){
+        ventana__edit.className = "ventana__edit_show";
+        // console.log('editar')
+        const id=e.target.parentNode.childNodes;
+        captura_id=id[2].id;
+        
+        console.log(captura_id)
+        
     }if(e.target.classList.value===aceptar){
-        console.log("no")
+        const el=elemento_temp;const lis=el.parentNode;lit=lis.parentNode;sect=lit.parentNode; 
+        sect.removeChild(lit)
+        ventana_eliminar.className = "default_eliminar";
+        
+    }if(e.target.classList.value===cancelar){
+        ventana_eliminar.className = "default_eliminar";
     }
+    if(e.target.classList.value===ventana__edit_guardar){
+        console.log('guardar')
+        
+        
+        const nombre=document.querySelector('#nombre').value;
+        const telefono=document.querySelector('#telefono').value;
+        const pais=document.querySelector('#pais').value;
+        const descripcion=document.querySelector('#descripcion').value;
+        // dogs[`${captura_id}`].name=nombre;
+        // dogs[`${captura_id}`].telefono=telefono;
+        // dogs[`${captura_id}`].pais=pais;
+        // dogs[`${captura_id}`].descripcion=descripcion;
+        // console.log(dogs)
+        contador++;
+        console.log(contador)
+        
+        
+        dogs.forEach(function(objeto) {
+            if (objeto.id == captura_id) {
+              objeto.nombre = nombre;
+              objeto.name=nombre;
+              objeto.telefono=telefono;
+              objeto.pais=pais;
+              objeto.descripcion=descripcion;
+            }
+          });
+          console.log(dogs)
+          render();
 
-    });
-
-const aceptar=document.querySelector('.aceptar'); 
-const cancelar=document.querySelector('.cancelar')
-const ventana_eliminar=document.querySelector('.default_eliminar')
-
-aceptar.addEventListener('click',function(e){
-    if (e.target.classList.value===boton_eliminar){
-        ventana_eliminar.className = "eliminar";
-    }else{
-        console.log("no")
+    }if(e.target.classList.value===ventana__edit_cancelar){
+        ventana__edit.className = "ventana__edit";  
     }
-});
-cancelar.addEventListener('click',function(e){
-  
+    
 });
 
-
-
-
-
+render();
 
